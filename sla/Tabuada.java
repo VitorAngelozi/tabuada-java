1ab5a76807dd94baf09d57cdad4e486f1be1448e
VitorAngelozi <vitorangelozi2013@gmail.com>
2025-10-29 20:48:55 -0300

    Adiciona projeto Tabuada em Java

---
+++ sla/Tabuada.java
@@ -0,0 +1,19 @@
+package sla;
+import java.util.Scanner;
+
+public class Tabuada {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite um número para ver a tabuada: ");
+        int numero = input.nextInt();
+
+        System.out.println("\nTabuada do " + numero + ":");
+        for (int i = 1; i <= 10; i++) {
+            int resultado = numero * i;
+            System.out.println(numero + " x " + i + " = " + resultado);
+        }
+
+        input.close();
+    }
+}
